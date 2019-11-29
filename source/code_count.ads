46c5d9a59c11a238df532f813964f8c63922728d
xiashuangxi <xiashuangxi@hotmail.com>
2019-11-29 14:47:08 +0800

    update

--- source/code_count.ads
+++ source/code_count.ads
@@ -6,32 +6,58 @@ package Code_Count is
     type CodeMate is private;
     type TotalCodeMate is private;
     
-    procedure Skip_Space (Line : String; 
-                        Offset : in out Integer);
+    -- Code_Count/Skip_Space
+    -- FUNCTION
     -- 跳过空格
-    -- @pram Line 字符串
-    -- @pram Offset 偏移量
+    -- PARAMETERS
+    -- Line   - 字符串
+    -- Offset - 偏移量
+    procedure Skip_Space 
+        (Line : String; 
+        Offset : in out Integer);
 
-    procedure Print_Core_Count (Count_Ident : String; 
-                            Count : Integer; 
-                            Number_Of_New_Lines : Ada.Text_IO.Count );
+    -- Code_Count/Print_Core_Count
+    -- FUNCTION
     -- 打印行数信息
-    -- @param Count_Ident 说明
-    -- @param Count 行数
-    -- @param Number_Of_New_Lines 换行数
-
-    procedure Read (FileName : String;
-                CM: in out CodeMate;
-                TM: in out TotalCodeMate);
+    -- PARAMETERS
+    -- Count_Ident          - 说明
+    -- Count                - 行数
+    -- Number_Of_New_Lines  - 换行数
+    procedure Print_Core_Count 
+        (Count_Ident : String; 
+        Count : Integer; 
+        Number_Of_New_Lines : Ada.Text_IO.Count );
+    
+    -- Code_Count/Read
+    -- FUNCTION
     -- 开始读取源码
+    -- PARAMETERS
+    -- FileName - 文件名
+    -- CM       - 当前文件的行数信息
+    -- TM       - 所有文件的行数信息
+    procedure Read 
+        (FileName : String;
+        CM: in out CodeMate;
+        TM: in out TotalCodeMate);
 
+    -- Code_Count/Print_To_File_Info
+    -- FUNCTION
+    -- 打印一个文件的信息
+    -- PARAMETERS
+    -- CM - 当前文件的行数信息
     procedure Print_To_File_Info(CM: in CodeMate);
+
+    -- Code_Count/Print_To_File_Info
+    -- FUNCTION
+    -- 打印所有文件的信息
+    -- PARAMETERS
+    -- TM - 所有文件的行数信息
     procedure Print_To_AllFile_Info(TM: in TotalCodeMate);
 
 private
 
-    File_Handle : Ada.Text_IO.File_Type;
     -- 文件操作对象
+    File_Handle : Ada.Text_IO.File_Type;
 
     type CodeCountMate is
         record
